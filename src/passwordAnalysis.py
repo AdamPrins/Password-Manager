b5a087b4c784263b93c8511578adea882e01e4e5
Adam Prins <AdamPrins@cmail.carleton.ca>
2021-02-24 22:44:56 -0500

    add password entropy function
    calculates password entropy
    uses entropy to score password (Weak, Reasonable, Strong)

--- src/passwordAnalysis.py
+++ src/passwordAnalysis.py
@@ -3,6 +3,12 @@ import secrets
 import string
 import requests
 import hashlib
+import math
+
+commonSymbols = "~!@#$%^&*_-+=,.?"
+brackets = "<>{}[]()"
+irregularSymbols = ";:\"\'"
+
 
 def generateMemorablePassword(length=4, hasCapitals=True, paddingType=list(string.digits)):
     """
@@ -11,16 +17,7 @@ def generateMemorablePassword(length=4, hasCapitals=True, paddingType=list(strin
     Words are separated by some padding character determined by padding type.
     """
 
-    wl = open('./dict/words', 'r')
-    fullWordlist = wl.readlines()
-    wl.close()
-
-    wordlist = []
-    for word in fullWordlist:
-        # Filters for words of length 4 or more,
-        # since words still have line break at the end
-        if len(word) > 4:
-            wordlist.append(word[:-1])
+    wordlist = getWordlist()
 
     # Selects which word should be in fullcaps
     if hasCapitals:
@@ -38,6 +35,8 @@ def generateMemorablePassword(length=4, hasCapitals=True, paddingType=list(strin
 
     return password
 
+
+
 def generatePassword(length=20, hasAlphabet=True, hasCapitals=True, hasDigits=True,
                     hasBasicSymbols=True, hasAdvancedSymbos=False):
     """
@@ -54,15 +53,17 @@ def generatePassword(length=20, hasAlphabet=True, hasCapitals=True, hasDigits=Tr
     if hasDigits:
         characterPool += list(string.digits)
     if hasBasicSymbols:
-        characterPool += list("!@#$%&*?")
+        characterPool += list(commonSymbols)
     if hasAdvancedSymbos:
-        characterPool += list("<>{}[](),.")
+        characterPool += list(brackets)
 
     password = ""
     for i in range(length):
         password += secrets.choice(characterPool)
 
     return password
+
+
 
 def isPasswordKnown(password):
     """
@@ -90,11 +91,115 @@ def passwordStrength(password):
     This determins the passowrd strength of the passed password
     """
     if isPasswordKnown(password)>0:
-        return "known Password"
+        return "Known Password"
     else:
-        return "TODO"
+        entropy = passwordEntropy(password)
+
+        if entropy<28:
+            return "Very Weak"
+        elif entropy<36:
+            return "Weak"
+        elif entropy<60:
+            return "Reasonable"
+        elif entropy<128:
+            return "Strong"
+        else:
+            return "Very Strong"
+
+
+
+def passwordEntropy(password):
+    """
+    Calcualtes the entropy of a password
+    """
+
+    charPool = 0
+    if any(c in password for c in string.ascii_lowercase):
+        charPool += len(string.ascii_lowercase)
+
+    if any(c in password for c in string.ascii_uppercase):
+        charPool += len(string.ascii_uppercase)
+
+    if any(c in password for c in string.digits):
+        charPool += len(string.digits)
+
+    if any(c in password for c in commonSymbols):
+        charPool += len(commonSymbols)
+
+    if any(c in password for c in brackets):
+        charPool += len(brackets)
+
+    if any(c in password for c in irregularSymbols):
+        charPool += len(irregularSymbols)
+
+    # Each character has a basic entroy as the size of the character pool
+    entropy = [charPool for i in range(len(password))]
+
+    # Repetitious characters have less entropy
+    for i in range(1,len(password)):
+        if password[i] == password[i-1]:
+            entropy[i] = entropy[i-1]/2
+
+
+    wordlist = getWordlist()
+    simplePass = password.lower()
+
+    # Iterates over possible character combinations, looking for words
+    # Starting with 4 characters, since <4 character words have higher entropy
+    # than the individual characters
+    for wordLength in range(4,len(password)+1):
+        for pos in range(0,len(password)-wordLength+1):
+            if (simplePass[pos:pos+wordLength] in wordlist):
+
+                # Words have an entropy equal to the common wordlist size (8000),
+                # spread over each character in the word
+                wordEntropy = 8000**(1.0/wordLength)
+                # Capitals increase the word entropy
+                if any(c in password[pos:pos+wordLength] for c in string.ascii_uppercase):
+                    wordEntropy*=2
+                # Selects the minimum entropy for a given character
+                for char in range(pos,pos+wordLength):
+                    entropy[char] = min(entropy[char], wordEntropy)
+
+    # Entropy is usually formatted as 2 to the power of the entropy equals the possibilities
+    return math.log2(prod(entropy))
+
+
+def prod(list):
+    """
+    Returns the product of a list
+    """
+    product = 1
+    for item in list:
+        product*=item
+
+    return product
+
+
+
+def getWordlist():
+    """
+    Returns the word wordlist
+    current wordlist coppied from usr/bin/share/dict/words
+    """
+    wl = open('./dict/words', 'r')
+    fullWordlist = wl.readlines()
+    wl.close()
+
+    wordlist = []
+    for word in fullWordlist:
+        # Filters for words of length 4 or more,
+        # since words still have line break at the end
+        if len(word) > 4:
+            wordlist.append(word[:-1])
+
+    return wordlist
 
 
 
 if __name__ == "__main__":
-    print(generateMemorablePassword())
+    passwords = ["password", "LittleRedridding", "123096521",
+        generateMemorablePassword(), generatePassword()]
+
+    for password in passwords:
+        print(password + ": " + passwordStrength(password))
