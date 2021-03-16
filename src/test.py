b71124cb939bac130960b28aade559c873f9b793
ryanabaker <39451853+ryanabaker@users.noreply.github.com>
2021-03-15 21:29:17 -0400

    encryption testing

--- src/test.py
+++ src/test.py
@@ -6,5 +6,13 @@ class TestSum(unittest.TestCase):
     def test_salt(self):
         self.assertEqual(len(encryption.generateSalt()), 16)
 
+    def test_encryption(self):
+
+        ciphertext = encryption.encrypt("i hate mushrooms but broccooli is cool", "super_bad_password")
+        self.assertEqual(type(ciphertext), type(b'bytes'))
+
+        plaintext = encryption.decrypt(ciphertext, "super_bad_password")
+        self.assertEqual(plaintext, "i hate mushrooms but broccooli is cool")
+
 if __name__ == '__main__':
     unittest.main()
